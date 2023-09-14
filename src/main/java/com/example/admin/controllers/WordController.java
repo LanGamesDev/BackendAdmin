ab776984b9b48786425f683bb573313f84432175
Javier Flores <jffc.dev@gmail.com>
2023-09-14 16:35:30 -0500

    feat: word controller added

---
+++ src/main/java/com/example/admin/controllers/WordController.java
@@ -0,0 +1,23 @@
+package com.example.admin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/words")
+public class WordController {
+    @Autowired
+    private WordRepository WordRepository;
+
+    @GetMapping
+    public List<Word> getAllWords() {
+        return WordRepository.findAll();
+    }
+
+    @PostMapping
+	public Word createWord(@RequestBody Word word) {
+		return WordRepository.save(word);
+	}
+}
