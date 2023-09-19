28b7776c466614f1670f31d999caa3cf7ad5288c
Javier Flores <jffc.dev@gmail.com>
2023-09-19 13:23:40 -0500

    feat: Put action added to words controller

--- src/main/java/com/langames/admin/controllers/WordController.java
+++ src/main/java/com/langames/admin/controllers/WordController.java
@@ -3,6 +3,8 @@ package com.langames.admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 
 @RestController
@@ -13,11 +15,22 @@ public class WordController {
 
     @GetMapping
     public List<Word> getAllWords() {
-        return WordRepository.findAll();
+        return WordRepository.findAllByOrderByIdAsc();
     }
 
     @PostMapping
 	public Word createWord(@RequestBody Word word) {
 		return WordRepository.save(word);
+	}
+
+    @PutMapping("/{id}")
+	public ResponseEntity<Word> updateWord(@PathVariable Long id, @RequestBody Word wordDetails){
+		Word _word = WordRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Word not exist with id :" + id));
+		
+		_word.setContent(wordDetails.getContent());
+		
+		Word updatedWord = WordRepository.save(_word);
+		return ResponseEntity.ok(updatedWord);
 	}
 }
