9273b2bb3dafc011b4bfa092350ccd91f5e9300e
Javier Flores <jffc.dev@gmail.com>
2023-09-19 13:24:04 -0500

    feat: findAllByOrderByIdAsc added to WordRepository

--- src/main/java/com/langames/admin/repositories/WordRepository.java
+++ src/main/java/com/langames/admin/repositories/WordRepository.java
@@ -3,6 +3,9 @@ package com.langames.admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WordRepository extends JpaRepository<Word, Long> {
+    public List<Word> findAllByOrderByIdAsc();
 }
