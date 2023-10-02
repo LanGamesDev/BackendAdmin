219ef05e1192be2862157820bb31939fbd7fa1a0
Javier Flores <jffc.dev@gmail.com>
2023-10-02 01:46:57 -0500

    feat: TranslateDAO file added

---
+++ src/main/java/com/langames/admin/entities/Translate/TranslateDAO.java
@@ -0,0 +1,11 @@
+package com.langames.admin.entities.Translate;
+
+import java.util.Date;
+
+public class TranslateDAO {
+
+    public long id;
+	public String content;
+	public Date fechaCreacion;
+
+}
