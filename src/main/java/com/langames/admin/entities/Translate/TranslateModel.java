9d05dfbcef2bd56a432a3e0585d67634d28d34ed
Javier Flores <jffc.dev@gmail.com>
2023-10-04 05:08:35 -0500

    feat: word reference added to TranslateModel

--- src/main/java/com/langames/admin/entities/Translate/TranslateModel.java
+++ src/main/java/com/langames/admin/entities/Translate/TranslateModel.java
@@ -1,6 +1,8 @@
 package com.langames.admin.entities.Translate;
 import java.util.Date;
 
+import com.langames.admin.entities.Word.WordModel;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -9,6 +11,10 @@ public class TranslateModel {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "word_id")
+    private WordModel word;
 	
 	@Column(name = "content")
 	private String content;
@@ -16,17 +22,23 @@ public class TranslateModel {
     @Column(name = "audi_fechaCreacion")
 	private Date fechaCreacion;
 
-	public TranslateModel() {
-		
-	}
+	public TranslateModel() {}
 	
-	public TranslateModel(String content) {
+	public TranslateModel(String content, WordModel word, Date fechaCreacion) {
 		super();
 		this.content = content;
+		this.word = word;
+		this.fechaCreacion = fechaCreacion;
 	}
 
 	public long getId() {
 		return id;
+	}
+	public void setWord(WordModel word) {
+		this.word = word;
+	}
+    public WordModel getWord() {
+		return word;
 	}
 	public void setId(long id) {
 		this.id = id;
