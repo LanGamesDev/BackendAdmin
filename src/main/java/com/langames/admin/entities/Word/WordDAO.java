0756c795a0211dec263ded6698a5dbef73c38568
Javier Flores <jffc.dev@gmail.com>
2023-10-04 19:56:15 -0500

    feat: translates added to WordDAO

--- src/main/java/com/langames/admin/entities/Word/WordDAO.java
+++ src/main/java/com/langames/admin/entities/Word/WordDAO.java
@@ -1,20 +1,24 @@
 package com.langames.admin.entities.Word;
 
+import java.util.List;
+
 import com.langames.admin.entities.Translate.TranslateDAO;
 
 public class WordDAO {
 
 	public long id;
 	public String content;
-    public TranslateDAO[] translates;
+    public List<TranslateDAO> translates;
 
 	public WordDAO() {
 		
 	}
 	
-	public WordDAO(String content) {
+	public WordDAO(long id, String content, List<TranslateDAO> translates) {
 		super();
+        this.id = id;
 		this.content = content;
+		this.translates = translates;
 	}
 
 }
